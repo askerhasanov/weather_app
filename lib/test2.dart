f500524b284a78e0ee9cb3f1cf175b40d7eed3df
Asker Hasanov <askerhasanov@gmail.com>
2023-11-20 19:13:14 +0400

    Create test2.dart

---
+++ lib/test2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Test2Github extends StatelessWidget {
+  const Test2Github({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
