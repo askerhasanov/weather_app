9c0747342022868956d064feba045549baa96b8f
Asker Hasanov <askerhasanov@gmail.com>
2023-11-20 19:09:13 +0400

    Update test.dart

---
+++ lib/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
